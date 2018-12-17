bc86ff423576be16445a85f8b8bfde582b80161c
rheakaru <rheakaru@stanford.edu>
2018-12-17 20:02:55 +0530

    playing around with javascript

--- helloWorld.js
+++ helloWorld.js
@@ -12,3 +12,19 @@ function helloWorld() {
     numTimes --;
   }
 }
+
+function header() {
+  var div = document.getElementById("test")
+  div.innerHTML = <header><div><a href=\"index.html\"><img border=\"0\" src=\"header.png\" align=\"middle\"></a></div><nav align=\"middle\"><ul><li> <a class= \"navText\" href=\"index.html\">Home</a> </li><li> <a class= \"navText\" href=\"act.html\">Activities</a> </li><li> <a class= \"navText\" href=\"cs.html\">Computer Science</a></li><li> <a class= \"navText\" href=\"writing.html\">Writing</a></li><li> <a class= \"navText\" href=\"art.html\">Design</a></li></ul></nav></header>";
+  var head = document.createElement("HEADER");                       // Create a <p> node
+  var div = document.createElement("DIV");      // Create a text node
+  head.appendChild(div);
+
+  //var para = document.createElement("P");                       // Create a <p> node
+//  var t = document.createTextNode("This is a paragraph.");      // Create a text node
+//  para.appendChild(t);
+  //div.appendChild(para)
+  document.body.prepend(head);                                // Append the text to <p>
+  //document.getElementById("test").appendChild(para);           // Append <p> to <div> with id="myDIV"
+    //"<header><div><a href=\"index.html\"><img border=\"0\" src=\"header.png\" align=\"middle\"></a></div><nav align=\"middle\"><ul><li> <a class= \"navText\" href=\"index.html\">Home</a> </li><li> <a class= \"navText\" href=\"act.html\">Activities</a> </li><li> <a class= \"navText\" href=\"cs.html\">Computer Science</a></li><li> <a class= \"navText\" href=\"writing.html\">Writing</a></li><li> <a class= \"navText\" href=\"art.html\">Design</a></li></ul></nav></header>")
+}
