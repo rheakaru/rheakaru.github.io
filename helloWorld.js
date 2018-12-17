fdb47bb3dfff076a3414a230aaa269d69868cbb7
rheakaru <rheakaru@stanford.edu>
2018-12-17 19:24:43 +0530

    airplane, playing around with sections, hovering

--- helloWorld.js
+++ helloWorld.js
@@ -1,4 +1,14 @@
 
 function helloWorld() {
-  document.write("<p> Hello VBScript!</p>")
+  //read in information from file
+  //get number of sections
+  //for each section:
+    //get info
+    //add to screen with appropriate
+  document.write("<img src=\"art/art-1.png\" class=\"artImg\"> Hello VBScript!</img> ");
+  var numTimes = 10;
+  while (numTimes > 0) {
+    document.write("<p>hello world</p>");
+    numTimes --;
+  }
 }
