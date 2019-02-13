5c731a7b63fa1e2d5f86e618cd57fbe55b46a84a
rheakaru <rheakaru@stanford.edu>
2019-02-13 02:09:56 -0800

    fixed writing page

--- writing.js
+++ writing.js
@@ -24,7 +24,7 @@ function loadWriting() {
     var div2= document.createElement("div")
     var header = document.createElement("h3")
     header.innerHTML = types[i]
-    div2.setAttribute("class", "col")
+    div2.setAttribute("style", "")
     div2.appendChild(header)
 
     div.appendChild(div2)
@@ -60,4 +60,49 @@ function loadWriting() {
   body.appendChild(big)
   }
 
+}
+
+function loadSimple() {
+  all = [{"title": "Stanford Daily: All", "link": "https://www.stanforddaily.com/?s=rhea+karuturi", "image": "daily.png", "type":"Journalism"},
+  {"title": "Bay City Beacon: Uber and Lyft", "link": "https://www.thebaycitybeacon.com/politics/background-checks-for-uber-and-lyft/article_8112ec2c-3be3-11e7-8e32-d3a18f6232ca.html", "image": "uber.png", "type":"Journalism"},
+  {"title": "Bay City Beacon: Internet in Juvenile Prisons", "link": "https://www.thebaycitybeacon.com/politics/ab---granting-the-right-to-internet-in-juvenile/article_09e90126-3bee-11e7-81e0-2b4920c85858.html", "image": "juvi.png","type":"Journalism"},
+{"title": "Deccan Herald", "link":"https://www.deccanherald.com/content/426276/artists-go-multisensorial-media-works.html" , "image": "artShow.png", "type":"Journalism"},
+{"title":"A Year in Being Indian", "link":"https://www.theodysseyonline.com/year-being-indian","image": "indian.png", "type":"Blogging"},
+{"title":"Being Seen", "link":"https://www.theodysseyonline.com/seeing-and-being-seen","image": "seen.png","type":"Blogging" },
+{"title":"On Rodin", "link":"https://www.theodysseyonline.com/year-being-indian","image":"rodin.png","type":"Blogging" },
+{"title": "India Article", "link": "https://www.facebook.com/stanford.global/posts/there-is-a-dichotomy-that-seems-sacred-between-the-individual-and-the-political-/1057096874334124/", "image": "verma.png", "type":"Media about me"},
+  {"title": "Writing Award", "link": "https://undergrad.stanford.edu/programs/introsems/teach/introsem-excellence-award/excellence-award-winners" , "image": "intro_excellence.png", "type":"Media about me"},
+  {"title": "The Million Page Project", "link": "https://www.deccanherald.com/content/476410/need-heard.html", "image": "tmpp.png", "type":"Media about me"},
+  {"title": "Kilimanjaro", "link": "https://bangaloremirror.indiatimes.com/opinion/sunday-read/on-top-of-the-world/articleshow/53890829.cms", "image": "killi.png", "type":"Media about me"},
+  {"title": "GSB Research", "link": "https://globalpoverty.stanford.edu/news/stanford-students-find-challenges-and-rewards-while-doing-field-research-center-faculty" , "image": "gsb.png", "type":"Media about me"}
+]
+  var body = document.getElementById("writingBody")
+  for(i=0; i< all.length;i++) {
+    var div = document.createElement("div")
+    div.setAttribute("class", "csBlock")
+
+    var title = document.createElement("p")
+    title.innerHTML = all[i]["title"]
+    title.setAttribute("href", all[i]["link"])
+    var p = document.createElement("p")
+    p.innerHTML = all[i]["type"]
+
+    var img =document.createElement("img")
+    img.setAttribute("src", all[i]["image"])
+    img.setAttribute("style", "width: 23vw;")
+    img.setAttribute("style", "height: 20vw;")
+
+
+    var a = document.createElement("a")
+    a.setAttribute("href", all[i]["link"])
+
+    a.appendChild(img)
+
+    div.appendChild(title)
+    title.appendChild(p)
+    title.appendChild(a)
+
+    body.appendChild(div)
+
+  }
 }
